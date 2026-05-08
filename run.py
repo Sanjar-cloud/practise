89ea0f712532b8d1e4de184068e5f0ba51c2f19d
sanjar <sanjarbekkarimov229@gmail.com>
2026-05-08 20:01:09 +0900

    feat: primitive variable

--- run.py
+++ run.py
@@ -1,5 +1,48 @@
-# message = "hello world!"
-# print(message)
+# Dunder __builtins_, _init_
 
-a = 100000
-print(a)
+message = "PYTHON: Everything is object!"
+print (message)
+
+result = type(message)
+print(" result:", result)
+
+''' In Python, there are builtin tools:
+(1) TYPES > int float str list dict
+(2) FUNCTIONS > print () len() input () type() str() int()
+(3) CONSTANTS > True False None
+'''
+
+print(dir(_builtins_))
+
+
+#////////////
+print("===== number =======")
+
+# in JAVA, variable is a name storage location!
+# in Python, variable is named reference!
+
+count = 100
+count_type = type(count)
+print(f"the count: {count} and type: {count_type}")
+
+result1 = count.bit_count() # method
+result2 = count. numerator # state
+print(result1, result2)
+
+
+# METHODS: upper() lower() title() find() replace()
+
+course = "AI Python FullStack"
+result = type(course)
+print(f"the result (1): {result}")
+
+result = course. title()
+print(f"the result (2): {result}")
+
+curse = course. replace("FullStack"
+print(f"the result (4): {result}")
+print course)
+
+
+## //////////
+
