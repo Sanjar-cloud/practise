29d8608def9cb4cd9cd4d1016fc9781bf7db070d
sanjar <sanjarbekkarimov229@gmail.com>
2026-05-18 18:03:08 +0900

    feat:packages and debugging

---
+++ debugging.py
@@ -0,0 +1,56 @@
+# Packages & Debugging
+# (1) Python Packages & Core Package
+# (2) Package Manager & External Package
+# (3) Debugging
+
+import turtle
+
+print("===== Python Packages & Core Package =====")
+# Python Packages/Modules: Core, File and External
+# Core Packages > https://docs.python.org/3/library
+
+# Core package
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.speed(2)
+# t.circle(150)
+# turtle.done()
+
+my_file = open("material/messagge.txt", "r")
+
+try:
+   content = my_file.read()
+   print("content:", content)
+finally:
+   my_file.close()
+
+# with
+with open("material/messagge.txt", "r") as your_file:
+   your_content = your_file.read()
+   print("your_content:", your_content)
+
+print("DONE")
+
+print("===== Package Manager & External Package =====")
+# Package Managers: pip pipenv npm yarn composer brew
+
+# External Package > https://pypi.org/
+# from PIL import Image
+
+# with Image.open("material/logo.png") as img_obj:
+#    resized_img = img_obj.resize((200, 200))
+#    resized_img.show()
+#    resized_img.save("material/")
+
+
+print("=======debugging=========")
+
+def get_summary(*args):  # DEFINE
+   total_amount = 0
+   for a in args:
+       total_amount += a
+   return total_amount  # solve the bug via debugging
+
+test = 100
+result = get_summary(1, 2, 3, 4, 5)  # CALL
+print("result:", result)
